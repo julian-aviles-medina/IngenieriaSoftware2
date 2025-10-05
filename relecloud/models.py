e982d5bf908251d2091f7a14b2afdb783b199560
Juldes125 <julavimed@gmail.com>
2025-10-05 22:37:15 +0200

    todo

---
+++ relecloud/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+class Cruise(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+class InfoRequest(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
---
+++ relecloud/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.hmtl")
+
+def destinations(request):
+    all_destinations = models.Destination.objects.all()
+    return render(request, 'destinations.html', { 'destinations': all_destinations})
+
+class DestinationDetailView(generic.DetailView):
+    template_name = 'destination_detail.html'
+    model = models.Destination
+    context_object_name = 'destination'
+
+    class CruiseDetailView(generic.DetailView):
+        template_name = 'cruise_detail.html'
+    model = models.Cruise
+    context_object_name = 'cruise'
+
+class InfoRequestCreate(SuccessMessageMixin, generic.CreateView):
+    template_name = 'info_request_create.html'
+    model = models.InfoRequest
+    fields = ['name', 'email', 'cruise', 'notes']
+    success_url = reverse_lazy('index')
+    success_message = 'Thank you, %(name)s! We will email you when we have more information about %(cruise)s!'
